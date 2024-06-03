591eb79bddd7c6af53b0c24ca2ee0ed274eb41dc
Anattam <102323636+Anattam@users.noreply.github.com>
2024-06-03 16:54:57 +0530

    Ok

--- server.js
+++ server.js
@@ -1,60 +1,59 @@
-var express= require('express');
-var app=express();
-var bodyParser= require('body-parser');
-var fs=require("fs");
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require("fs");
 var multer = require("multer");
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static('public'));
-app.use(multer({dest:'/tmp/'}));
-
-app.post('/file_upload', function (req,res) {
-    console.log(req.files.file.name);
-    console.log(req.files.file.path);
-    console.log(req.files.file.type);
-    
-   var file=__dirname+"/"+req.files.file.name;
-
-   fs.readFile(req.files.file.path, function (err,data) {
-    fs.writeFile(file,data,function (err) {
-        if (err) {
-            console.log(err);   
-        }else{
-            response={
-                message:"file uploaded",
-                filename:req.files.file.name
-            };
-        }
-        console.log(response);
-        res.end(JSON.stringify(response));
-    });
-   });
+// Set up storage configuration for multer
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/tmp/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
 });
 
-app.delete('/del_user', function (req,res) {
+var upload = multer({ storage: storage });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.post('/file_upload', upload.single('file'), function (req, res) {
+    console.log(req.file.originalname);
+    console.log(req.file.path);
+    console.log(req.file.mimetype);
+
+    var file = __dirname + "/" + req.file.originalname;
+
+    fs.readFile(req.file.path, function (err, data) {
+        fs.writeFile(file, data, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                response = {
+                    message: "File uploaded successfully",
+                    filename: req.file.originalname
+                };
+            }
+            console.log(response);
+            res.end(JSON.stringify(response));
+        });
+    });
+});
+
+app.delete('/del_user', function (req, res) {
     console.log("Got a DELETE request");
     res.send("Hello DELETE");
 });
 
-app.get('/index.htm', function (req,res) {
-    res.sendFile(__dirname+"/"+"index.htm");
+app.get('/index.htm', function (req, res) {
+    res.sendFile(__dirname + "/" + "index.htm");
 });
 
-/*app.get('/process_get', function (req,res) {
-    response={
-        first_name:req.query.first_name,
-        last_name:req.query.last_name
-    };
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
-    console.log(response);
-    res.send(JSON.stringify(response));
+    console.log("Example app listening at http://%s:%s", host, port);
 });
-*/
-var server= app.listen(8081, function () {
-    var host= server.address().address;
-    var port= server.address().port;
-
-    console.log("Example app listening at http:// %s : %s ", host, port);
-});
-
-
