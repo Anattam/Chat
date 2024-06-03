e95007bbf1c5c6c4abb014e696d567b25ab3de4a
Anattam <102323636+Anattam@users.noreply.github.com>
2024-06-03 16:51:38 +0530

    Done

--- server.js
+++ server.js
@@ -1,27 +1,60 @@
-var http= require('http')
-var fs= require('fs')
-var url= require('url')
+var express= require('express');
+var app=express();
+var bodyParser= require('body-parser');
+var fs=require("fs");
+var multer = require("multer");
 
-http.createServer(
-    function (request,response) {
-        var pathname= url.parse(request.url).pathname;
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static('public'));
+app.use(multer({dest:'/tmp/'}));
 
-        console.log("Request for "+pathname+ "received");
+app.post('/file_upload', function (req,res) {
+    console.log(req.files.file.name);
+    console.log(req.files.file.path);
+    console.log(req.files.file.type);
+    
+   var file=__dirname+"/"+req.files.file.name;
 
-        fs.readFile(pathname.substr(1), function (err,data) {
-            if (err) {
-                console.log(err);
-                response.writeHead(404,{"Content-Type":"text/html"});
-            }
-            else{
-                response.writeHead(200,{"Content-Type":"text/html"});
+   fs.readFile(req.files.file.path, function (err,data) {
+    fs.writeFile(file,data,function (err) {
+        if (err) {
+            console.log(err);   
+        }else{
+            response={
+                message:"file uploaded",
+                filename:req.files.file.name
+            };
+        }
+        console.log(response);
+        res.end(JSON.stringify(response));
+    });
+   });
+});
 
-                response.write(data.toString())
-            }
+app.delete('/del_user', function (req,res) {
+    console.log("Got a DELETE request");
+    res.send("Hello DELETE");
+});
 
-            response.end();
-        } )
-    }
-).listen(8081);
+app.get('/index.htm', function (req,res) {
+    res.sendFile(__dirname+"/"+"index.htm");
+});
 
-console.log("Server running at http://127.0.0.1:8081/");
+/*app.get('/process_get', function (req,res) {
+    response={
+        first_name:req.query.first_name,
+        last_name:req.query.last_name
+    };
+
+    console.log(response);
+    res.send(JSON.stringify(response));
+});
+*/
+var server= app.listen(8081, function () {
+    var host= server.address().address;
+    var port= server.address().port;
+
+    console.log("Example app listening at http:// %s : %s ", host, port);
+});
+
+
