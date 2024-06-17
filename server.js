2b6eedd48ba4a921df7ed72e0ff0419fef1f17e6
Anattam <102323636+Anattam@users.noreply.github.com>
2024-06-17 17:02:18 +0530

    Step 1

--- server.js
+++ server.js
@@ -1,59 +1,27 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var fs = require("fs");
-var multer = require("multer");
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
-// Set up storage configuration for multer
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, '/tmp/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const port = 3000;
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
 });
 
-var upload = multer({ storage: storage });
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'));
-
-app.post('/file_upload', upload.single('file'), function (req, res) {
-    console.log(req.file.originalname);
-    console.log(req.file.path);
-    console.log(req.file.mimetype);
-
-    var file = __dirname + "/" + req.file.originalname;
-
-    fs.readFile(req.file.path, function (err, data) {
-        fs.writeFile(file, data, function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                response = {
-                    message: "File uploaded successfully",
-                    filename: req.file.originalname
-                };
-            }
-            console.log(response);
-            res.end(JSON.stringify(response));
-        });
-    });
-});
-
-app.delete('/del_user', function (req, res) {
-    console.log("Got a DELETE request");
-    res.send("Hello DELETE");
-});
-
-app.get('/index.htm', function (req, res) {
-    res.sendFile(__dirname + "/" + "index.htm");
-});
-
-var server = app.listen(8081, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log("Example app listening at http://%s:%s", host, port);
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
