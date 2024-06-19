f658b1d3c125a7509517834245fbb6cdb4144071
Anattam <102323636+Anattam@users.noreply.github.com>
2024-06-19 18:44:59 +0530

    Server.js

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/chat", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Define the message schema and model
+const messageSchema = new mongoose.Schema({
+  username: String,
+  message: String,
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Message = mongoose.model("Message", messageSchema);
 
 const app = express();
 const server = http.createServer(app);
@@ -10,10 +29,26 @@ const port = 3000;
 
 app.use(express.static("public"));
 
+// Load messages from the database when the server starts
+async function loadMessages() {
+  const messages = await Message.find().sort("timestamp");
+  return messages;
+}
+
 io.on("connection", (socket) => {
   console.log("A user connected");
 
-  socket.on("chat message", (msg) => {
+  // Send previous messages to the newly connected client
+  loadMessages().then((messages) => {
+    socket.emit("previousMessages", messages);
+  });
+
+  socket.on("chat message", async (msg) => {
+    // Save the new message to the database
+    const message = new Message(msg);
+    await message.save();
+
+    // Broadcast the new message to all clients
     io.emit("chat message", msg);
   });
 
